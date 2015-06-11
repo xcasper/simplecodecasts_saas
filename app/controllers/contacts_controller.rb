3386d4a5f6627f1f3c47251a2ef637ec8a90f296
Craig Gleckman <cgleckman@gmail.com>
2015-06-11 21:08:58 +0000

    Added validations and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,20 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      flash[:success] = 'Message sent.'
+      redirect_to new_contact_path, notice: "Message sent."
+    else
+      # use of danger (aka error) for bootstrap class formatting in application.html.erb
+      flash[:danger] = 'Error occured, message has not been sent.'  
+      redirect_to new_contact_path, notice: "Error occured."
+    end
   end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
