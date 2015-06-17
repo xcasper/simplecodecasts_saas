ca9f2642d8d164e99a54eff7ffe85683e19d553d
Craig Gleckman <cgleckman@gmail.com>
2015-06-17 20:17:16 +0000

    recreating previous work after reverting code to resolve issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615031133) do
+ActiveRecord::Schema.define(version: 20150617193049) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
